e05b7bb8aa450cd19a11d7d6936a2aa57015e76d
Nurlan <nurlan264@mail.ru>
2024-04-10 01:15:27 +0300

    last commit

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -11,9 +11,10 @@ import java.time.format.DateTimeFormatter;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class CardDelivery {
-    private String generateDate(int addDays, String pattern) {
-        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+public class CardDeliveryTest {
+
+    private String generateDate() {
+        return LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
 
     }
 
@@ -22,7 +23,7 @@ public class CardDelivery {
 
         open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue("Москва");
-        String meetingDate = generateDate(3, "dd.MM.yyyy");
+        String meetingDate = generateDate();
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
         $("[data-test-id='date'] input").setValue(meetingDate);
         $("[data-test-id='name'] input").setValue("Иванов-Петров Иван");
